b4209d255ee53c03e3401b34496082c30f75e931
Hapnees <8dwardm8lton@gmail.com>
2023-05-28 13:17:38 +0300

    Fix (next-i18next.config): routes don't work

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,20 +1,24 @@
 module.exports = {
-    i18n: {
-        defaultLocale: 'ru',
-        locales: ['ru', 'en'],
-        localeDetection: false,
-    },
-    reloadOnPrerender: true,
-    ns: [
-        'header',
-        'auth_modal',
-        'home',
-        'common',
-        'footer',
-        'error',
-        'movies',
-        'admin',
-        'watch',
-        'review',
-    ],
-};
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru', 'en'],
+    localeDetection: false,
+  },
+  reloadOnPrerender: true,
+  ns: [
+    'header',
+    'auth_modal',
+    'home',
+    'common',
+    'footer',
+    'error',
+    'movies',
+    'admin',
+    'watch',
+    'review',
+  ],
+  localePath:
+    typeof window === 'undefined'
+      ? require('path').resolve('./public/locales')
+      : '/locales',
+}
